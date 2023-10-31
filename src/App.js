59d17138e27b005b406e25dd2c56e4d5eed761a5
Tejaswini Malla <tejaswini.chinni.143@gmail.com>
2023-10-31 18:18:58 +0530

    FirstCommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react'
+const App = ()=>{
+  const [data,setData] = useState({
+    username : '',
+    password : ''
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  })
+  const {username,password} = data;
+  const changeHandler = e => {
+    setData({...data, [e.target.name] : [e.target.value]})
+
+  }
+  const submitHandler = e => {
+    e.preventDefault()
+    console.log(data)
+  }
+  return(
+    <div>
+      <center>
+           <form onSubmit={submitHandler}>
+            <input type="text" name="username" value={username} onChange={changeHandler}/> <br/>
+            <input type="password" name="password" value={password} onChange={changeHandler}/> <br/>
+            <button>Submit</button>
+           </form>
+      </center>
     </div>
-  );
+  )
 }
-
-export default App;
+export default App
